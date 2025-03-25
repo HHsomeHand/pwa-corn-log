0c5f4955e04cd0d3bc40590955e10fdcbff36965
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-03-25 22:14:40 +0800

    feat: 添加了虚拟列表
    - 添加了虚拟列表

---
+++ src/components/CornPowerfulList.vue
@@ -0,0 +1,33 @@
+<script setup>
+let list = ref([]);
+for (let i = 1; i <= 399; i++) {
+  list.value.push({ id: i - 1, name: `hzx${i}` });
+}
+</script>
+
+<template>
+  <RecycleScroller
+      class="scroller"
+      :items="list"
+      :item-size="32"
+      key-field="id"
+      v-slot="{ item }"
+  >
+    <div class="user">
+      {{ item.name }}
+    </div>
+  </RecycleScroller>
+</template>
+
+<style scoped>
+.scroller {
+  height: 100%;
+}
+
+.user {
+  height: 32%;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+}
+</style>
