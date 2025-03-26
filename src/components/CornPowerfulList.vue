becf0bda8b0826649a361076c67a0e08d5bca4af
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-03-26 21:26:12 +0800

    refactor(超级滚动条): 超级滚动条重构
    - 抽取了 hook

--- src/components/CornPowerfulList.vue
+++ src/components/CornPowerfulList.vue
@@ -1,170 +1,29 @@
 <script setup>
 import { vScroll } from '@vueuse/components'
 import {useLogStore} from "@/store/logs.store.js";
-import {useEventListener, useScroll} from "@vueuse/core";
-import {throttle} from "underscore";
-import {fmtDate, getDomElement, vh2px} from "@/utils/index.js";
+import {fmtDate} from "@/utils/index.js";
 import {useStoreUtils} from "@/hooks/storeUtils.hook.js";
+import {useUpdateLogs} from "@/hooks/updateLogs.hook.js";
 
 const logsCache = ref([])
 
 const store = useLogStore()
 
-const  {getLogs, getLogsWithSeparator, generatorSeparator} = useStoreUtils(store);
-
-// 初始化日志缓存, 不带分隔符
-async function initLogs() {
-  let logs = await getLogs(new Date());
-
-  logsCache.value.push(...logs);
-}
-
-let _update = () => {};
-
-// TODO: 删掉
-async function onClick() {
-  // store.generateTestData();
-  _update();
-}
+const storeUtils = useStoreUtils(store);
 
 const scrollerRef = ref(null)
 
-let stopTouchStart = () => {};
-
-let stopTouchMove = () => {};
-
-onUnmounted(() => {
-  stopTouchStart();
-
-  stopTouchMove();
-})
-
-onMounted(async () => {
-  const scrollerEl = getDomElement(scrollerRef.value);
-
-  async function init() {
-    await initLogs();
-
-    await nextTick(() => {
-      scrollerEl.scrollTop = scrollerEl.scrollHeight; // 滚动到底部
-    });
-  }
-
-  await init();
-
-  // 实现触顶刷新
-  async function enableTopUpdate() {
-    const { arrivedState } = useScroll(scrollerEl);
-
-    const { top: isTop} = toRefs(arrivedState);
-
-    let startY = 0;
-
-    let isAtTopOnTouchStart;
-    stopTouchStart = useEventListener(scrollerEl, 'touchstart', (event) => {
-      startY = event.touches[0].pageY;
-
-      isAtTopOnTouchStart = isTop.value;
-    });
-
-    stopTouchMove = useEventListener(scrollerEl, 'touchmove', (event) => {
-      const currentY = event.touches[0].pageY;
-      const deltaY = currentY - startY;
-
-      // 只在顶部且向下拖动超过阈值时阻止默认行为
-      if (isAtTopOnTouchStart && deltaY > 0) {
-        event.preventDefault();
-      }
-
-      onScroll(deltaY);
-    },  { passive: false });
-
-    const onScroll = throttle((deltaY) => {
-      if (deltaY < 0) {
-        // 向上拖动会到这里, 这里要向下拖动才加载 logs
-        return;
-      }
-
-      // 如果不在顶部, 就不做处理
-      if (!isTop.value) {
-        return;
-      }
-
-      // console.log("逻辑");
-      _update();
-
-    }, 500);
-  }
-
-  await enableTopUpdate();
-
-  _update = async () => {
-    let date = new Date(logsCache.value[0].date);
-
-    if (logsCache.value[0].type !== "separator") {
-      logsCache.value.unshift(generatorSeparator(date));
-    }
-
-    date.setDate(date.getDate() - 1);
-
-    let logs = await getLogsWithSeparator(date);
-
-    const prevScrollTop = scrollerEl.scrollTop;
-    const prevScrollHeight = scrollerEl.scrollHeight;
-
-    logsCache.value.unshift(...logs);
-
-    function setScrollPos() {
-      // 计算新内容的增量高度
-      const heightDelta = scrollerEl.scrollHeight - prevScrollHeight;
-      // 保持用户视角不变：原偏移 + 新增高度
-      scrollerEl.scrollTop = prevScrollTop + heightDelta;
-    }
-
-    await nextTick(() => {
-      setScrollPos();
-    });
-  }
-});
-
-function onScroll(state) {
-  let {y} = state;
-
-  if (y.value < vh2px(50)) {
-    // console.log("逻辑");
-    _update();
-  }
-}
-
-// 渲染时动态分隔
-// const getSeparator = (index) => {
-//   if (index === 0) return null;
-//
-//   const currentDate = logsCache.value[index]?.date;
-//   if (!currentDate) return null;
-//
-//   const prevDate = logsCache.value[index - 1]?.date;
-//   if (!prevDate) return null;
-//
-//   return currentDate?.getTime?.() !== prevDate?.getTime?.() ? currentDate : null;
-// };
+const {onScroll} = useUpdateLogs(scrollerRef, storeUtils, logsCache);
 </script>
 
 <template>
   <div class="h-full flex flex-col">
-    <button @click="onClick">
-      click me
-    </button>
     <ul ref="scrollerRef" class="overflow-auto" v-scroll="onScroll">
       <template v-for="item in logsCache">
-        <template v-if="item.type==='separator'">
-          <li>separator {{fmtDate(item.date)}}</li>
-        </template>
-        <template v-else>
-          <li class="item">
+          <li v-if="item.type==='separator'">separator {{fmtDate(item.date)}}</li>
+          <li v-else class="item">
             {{ item.log + fmtDate(item.date)}}
           </li>
-        </template>
       </template>
 
     </ul>
--- src/hooks/updateLogs.hook.js
+++ src/hooks/updateLogs.hook.js
@@ -1,5 +1,130 @@
+import {getDomElement, vh2px} from "@/utils/index.js";
+import {useEventListener, useScroll} from "@vueuse/core";
+import {throttle} from "underscore";
 
 
-export function useUpdateLogs() {
+// 需要指定 scrollerRef 容器, 进行滚动监听
+// logsCache 需要为 ref
+export function useUpdateLogs(scrollerRef, storeUtils, logsCache) {
+    const {getLogs, getLogsWithSeparator, generatorSeparator} = storeUtils;
 
+    let update = () => {};
+
+    let stopTouchStart = () => {};
+
+    let stopTouchMove = () => {};
+
+    onUnmounted(() => {
+        stopTouchStart();
+
+        stopTouchMove();
+    })
+
+    onMounted(async () => {
+        const scrollerEl = getDomElement(scrollerRef.value);
+
+        // 初始化日志缓存, 不带分隔符
+        async function initLogs() {
+            let logs = await getLogs(new Date());
+
+            logsCache.value.push(...logs);
+        }
+
+        async function init() {
+            await initLogs();
+
+            await nextTick(() => {
+                scrollerEl.scrollTop = scrollerEl.scrollHeight; // 滚动到底部
+            });
+        }
+        await init();
+
+        // 实现
+        // - 触顶刷新
+        // - 初始页面无滚动条, 上拉依旧刷新
+        async function enableTopUpdate() {
+            const { arrivedState } = useScroll(scrollerEl);
+
+            const { top: isTop} = toRefs(arrivedState);
+
+            let startY = 0;
+
+            let isAtTopOnTouchStart;
+            stopTouchStart = useEventListener(scrollerEl, 'touchstart', (event) => {
+                startY = event.touches[0].pageY;
+
+                isAtTopOnTouchStart = isTop.value;
+            });
+
+            stopTouchMove = useEventListener(scrollerEl, 'touchmove', (event) => {
+                const currentY = event.touches[0].pageY;
+                const deltaY = currentY - startY;
+
+                // 只在顶部且向下拖动超过阈值时阻止默认行为
+                if (isAtTopOnTouchStart && deltaY > 0) {
+                    event.preventDefault();
+                }
+
+                onScroll(deltaY);
+            },  { passive: false });
+
+            const onScroll = throttle((deltaY) => {
+                if (deltaY < 0) {
+                    // 向上拖动会到这里, 这里要向下拖动才加载 logs
+                    return;
+                }
+
+                // 如果不在顶部, 就不做处理
+                if (!isTop.value) {
+                    return;
+                }
+
+                // 触顶时向上拉动走这里
+                update();
+
+            }, 500);
+        }
+        await enableTopUpdate();
+
+        update = async () => {
+            let date = new Date(logsCache.value[0].date); // 获取最新一条的日期
+
+            // 如果不是分割符, 就添加分割符
+            if (logsCache.value[0].type !== "separator") {
+                logsCache.value.unshift(generatorSeparator(date));
+            }
+
+            // 获取前一天的日期
+            date.setDate(date.getDate() - 1);
+
+            let logs = await getLogsWithSeparator(date);
+
+            const prevScrollTop = scrollerEl.scrollTop;
+            const prevScrollHeight = scrollerEl.scrollHeight;
+
+            logsCache.value.unshift(...logs);
+
+            function setScrollPos() {
+                // 计算新内容的增量高度
+                const heightDelta = scrollerEl.scrollHeight - prevScrollHeight;
+                // 保持用户视角不变：原偏移 + 新增高度
+                scrollerEl.scrollTop = prevScrollTop + heightDelta;
+            }
+
+            await nextTick(() => {
+                setScrollPos();
+            });
+        }
+    });
+
+    function onScroll(state) {
+        let {y} = state;
+
+        if (y.value < vh2px(50)) {
+            // console.log("逻辑");
+            update();
+        }
+    }
+
+    return {onScroll};
 }
