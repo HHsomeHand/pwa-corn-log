50a77bb36cff4936892b7d591a15a5804df63187
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-03-27 13:25:16 +0800

    wip(超级滚动条): css 重构中

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hj5imk06ri"
+    "revision": "0.1o4kat5p5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import router from './router'
 
 import VueVirtualScroller from 'vue-virtual-scroller'
 
-// 防止 showToast 没有样式: https://juejin.cn/post/7244335987576733752
-import 'vant/lib/index.css';
-
-// https://github.com/Akryum/vue-virtual-scroller/blob/master/packages/vue-virtual-scroller/README.md
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+// // 防止 showToast 没有样式: https://juejin.cn/post/7244335987576733752
+// import 'vant/lib/index.css';
+//
+// // https://github.com/Akryum/vue-virtual-scroller/blob/master/packages/vue-virtual-scroller/README.md
+// import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
 import { createPinia } from 'pinia'
 
