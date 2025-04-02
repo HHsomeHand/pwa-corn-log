1af4ffe9acd3e4fb658a4331e3ac408ab832ba54
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-04-02 22:19:13 +0800

    refactor: app.store.js 执行了重构, 把逻辑抽取到了 store 中, 方便全局共享

--- src/store/app.store.js
+++ src/store/app.store.js
@@ -1,6 +1,60 @@
 import {defineStore} from "pinia";
+import {CUSTOM_ORANGE_HEX, VANT_COLOR_VARS, vantVarName2ColorHex} from "@/color/vant.color.js";
+import {useConfig} from "@/hooks/useConfig.js";
+import {useCssVar} from "@/hooks/useCssVar.js";
 
 
 export const useAppStore = defineStore('appStore', () => {
 
+    const APP_MODE = Object.freeze({
+        LOG: {
+            title: "日志",
+            defaultPrimaryColor: CUSTOM_ORANGE_HEX,
+        },
+        LOVE: {
+            title: "善举",
+            defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.RED),
+        },
+        DRUG: {
+            title: "药律",
+            defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.BLUE),
+        },
+        TREATMENT: {
+            title: "戒律",
+            defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.GREEN),
+        }
+    });
+
+    const customAppMode = useConfig('customAppMode', {...APP_MODE});
+
+    // 不要直接修改, 使用 changeAppMode 来修改
+    const currentModeKey = useConfig('currentMode', 'LOG');
+
+    const currentMode = computed(() => {
+        return {
+            ...APP_MODE[currentModeKey.value],
+        }
+    })
+
+    const PRIMARY_COLOR_VAR_NAME = '--van-primary-color';
+
+    const primaryColor = useCssVar(PRIMARY_COLOR_VAR_NAME, CUSTOM_ORANGE_HEX);
+
+    watch(primaryColor, () => {
+        customAppMode.value[currentModeKey.value].customPrimaryColor = primaryColor.value;
+    });
+
+    // changeAppMode(APP_MODE.LOG)
+    function changeAppMode(key) {
+        let mode = customAppMode.value[key];
+        currentModeKey.value = key;
+        primaryColor.value = mode.customPrimaryColor || mode.defaultPrimaryColor;
+    }
+
+    return {
+        APP_MODE,
+        changeAppMode,
+        primaryColor,
+        currentMode
+    }
 });
--- src/view/ConfigView/ConfigView.vue
+++ src/view/ConfigView/ConfigView.vue
@@ -5,6 +5,8 @@ import colorValues, {CUSTOM_ORANGE_HEX, VANT_COLOR_VARS, vantVarName2ColorHex} f
 import {useCssVar} from "@/hooks/useCssVar.js";
 import {useConfig} from "@/hooks/useConfig.js";
 import {showActionSheet} from "@/components/CornActionSheet/utils.js";
+import {useAppStore} from "@/store/app.store.js";
+import {storeToRefs} from "pinia";
 
 function onClickLeft() {
   history.back();
@@ -12,57 +14,16 @@ function onClickLeft() {
 
 const colors = ref([CUSTOM_ORANGE_HEX, ...Object.values(colorValues)]);
 
-const APP_MODE = Object.freeze({
-  LOG: {
-    title: "日志",
-    defaultPrimaryColor: CUSTOM_ORANGE_HEX,
-  },
-  LOVE: {
-    title: "善举",
-    defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.RED),
-  },
-  DRUG: {
-    title: "药律",
-    defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.BLUE),
-  },
-  TREATMENT: {
-    title: "戒律",
-    defaultPrimaryColor: vantVarName2ColorHex(VANT_COLOR_VARS.GREEN),
-  }
-});
+const appStore = useAppStore();
 
-const customAppMode = useConfig('customAppMode', {...APP_MODE});
-
-// 不要直接修改, 使用 changeAppMode 来修改
-const currentModeKey = useConfig('currentMode', 'LOG');
-
-const currentMode = computed(() => {
-  return {
-    ...APP_MODE[currentModeKey.value],
-  }
-})
-
-const PRIMARY_COLOR_VAR_NAME = '--van-primary-color';
-
-const primaryColor = useCssVar(PRIMARY_COLOR_VAR_NAME, CUSTOM_ORANGE_HEX);
-
-watch(primaryColor, () => {
-  customAppMode.value[currentModeKey.value].customPrimaryColor = primaryColor.value;
-});
-
-// changeAppMode(APP_MODE.LOG)
-function changeAppMode(key) {
-  let mode = customAppMode.value[key];
-  currentModeKey.value = key;
-  primaryColor.value = mode.customPrimaryColor || mode.defaultPrimaryColor;
-}
+const {currentMode, primaryColor} = storeToRefs(appStore);
 
 function onModeCellClick() {
-  let actions = Object.entries(APP_MODE).map(([key, mode]) => {
+  let actions = Object.entries(appStore.APP_MODE).map(([key, mode]) => {
     return {
       name: mode.title,
       callback(item) {
-        changeAppMode(key);
+        appStore.changeAppMode(key);
       },
     } // END OF RETURN
   });
